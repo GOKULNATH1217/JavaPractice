6f9d8fb201f3a1a9f6f11e0dcf42c19a1ad9580d
Gokulnath S <gokulnath1217@gmail.com>
2026-07-15 17:12:43 +0530

    student constructor

--- git.java
+++ git.java
@@ -1,5 +1,24 @@
-public class git {
+class Student
+{
+    String name;
+    int age;
+    String dept;
+    Student(String name,int age,String dept)
+    {
+        this.name=name;
+        this.age=age;
+        this.dept=dept;
+    }
+    void display()
+    {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(dept);
+    }
+}
+public class Git {
     public static void main(String[] args) {
-        System.out.println("LEARNING GIT");
+        Student s1=new Student("GOkulnath",20,"CSE");
+        s1.display();
     }
 }
