c3e5278aa5755e3c7375bf13a11f1be243cf9335
Gokulnath S <gokulnath1217@gmail.com>
2026-07-29 21:09:04 +0530

    Add interface example

---
+++ Interface.java
@@ -0,0 +1,27 @@
+// class power
+// {
+//     void power()
+//     {
+//         System.out.println("POWER ON");
+//     }
+// }
+interface Vehicle {
+    void start();
+}
+class car implements Vechile{
+    public void start()
+    {
+        System.out.println("CAR STARTED");
+    }
+}
+class bike implements Vechile{
+    public void start()
+    {
+        System.out.println("BIKE STARTED");
+    }
+}
+public class Interface {
+    public static void main(String[] args) {
+        
+    }
+}
